151107ee3fef23ab1b8c62f027c89b7768b9d212
srs328 <srs32896@gmail.com>
2024-10-20 16:31:58 -0400

    deleted prepare_dataset() and power_of_two()

--- src/monai_training/preprocess.py
+++ src/monai_training/preprocess.py
@@ -207,9 +207,6 @@ class DataSetProcesser:
         self.dataset = dataset_copy
 
 
-def power_of_two(i: int) -> int:
-    return 2**i
-
 
 def save_dataset(
     dataset: DataSet, save_path: Path | os.PathLike, info: Optional[dict] = None
