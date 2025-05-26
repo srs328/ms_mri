f772ba9ced570f3f09eab4b9bd14f90614435d66
srs328 <srs32896@gmail.com>
2025-05-26 18:25:14 -0400

    longitudinal pipeline

--- choroid_thalamus_project/scripts/process_aseg.py
+++ choroid_thalamus_project/scripts/process_aseg.py
@@ -3,9 +3,10 @@ import json
 import os
 import shutil
 import subprocess
+from tqdm import tqdm
 
 fastsurfer_home = Path("/media/smbshare/srs-9/fastsurfer")
-hipsthomas_home = Path("/media/smbshare/srs-9/hipsthomas")
+hipsthomas_home = Path("/media/smbshare/srs-9/thalamus_project/data")
 dataroot = Path("/media/smbshare/3Tpioneer_bids")
 
 with open(dataroot / "subject-sessions-longit.json", 'r') as f:
@@ -15,14 +16,22 @@ with open(dataroot / "subject-sessions-longit.json", 'r') as f:
 subjects = [2195, 1076, 1042, 1508, 1071, 1241, 1003, 1301, 1001, 1107, 1125, 1161, 1198, 1218, 1527, 1376, 2075, 1023, 1038, 1098]
 subjects = [str(subid) for subid in subjects]
 
+fastsurfer_to_subject_space = "/home/srs-9/Projects/ms_mri/scripts/fastsurfer/fastsurfer_to_subject_space.sh"
 script = "/home/srs-9/Projects/ms_mri/choroid_thalamus_project/scripts/processAseg.sh"
 
-for subid in subjects:
+# for subid in subjects:
+for subid in tqdm(subject_sessions, total=len(subject_sessions)):
     sessions = sorted(subject_sessions[subid])
     sesid = sessions[0]
 
     fastsurfer_dir = fastsurfer_home / f"sub{subid}-{sesid}" / subid
-    assert (fastsurfer_dir / "mri").exists()
+    print(fastsurfer_dir)
+
+    if not (fastsurfer_dir / "mri").exists():
+        continue
+    if not (fastsurfer_dir / "mri" / "freesurfer-to-subject.mat").exists():
+        cmd = ["bash", fastsurfer_to_subject_space, fastsurfer_dir.parent, str(subid)]
+        subprocess.run(cmd)
 
     hipsthomas_dir = hipsthomas_home / f"sub{subid}-{sesid}"
 
