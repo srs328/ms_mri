3d803310ed5fad0daf2696439c25e85c7f9aff5f
srs328 <srs32896@gmail.com>
2025-11-05 14:37:14 -0500

    longitudinal hips thomas

--- choroid_thalamus_project/longitudinal_pipeline/run_hipsthomas_all_timepoints.py
+++ choroid_thalamus_project/longitudinal_pipeline/run_hipsthomas_all_timepoints.py
@@ -10,12 +10,13 @@ from loguru import logger
 from tqdm import tqdm
 
 #%%
-drive_root = fm.get_drive_root()
+# drive_root = fm.get_drive_root()
+drive_root = Path("/mnt/g")
 work_home = drive_root / "srs-9/longitudinal"
 dataroot = drive_root / "3Tpioneer_bids"
 
-work_home = Path("/media/smbshare/srs-9/longitudinal")
-dataroot = Path("/media/smbshare/3Tpioneer_bids")
+work_home = Path("/mnt/g/Data/longitudinal")
+dataroot = Path("/mnt/g/Data/3Tpioneer_bids")
 
 with open("/home/srs-9/Projects/ms_mri/data/subject-sessions-longit.json", 'r') as f:
     subject_sessions = json.load(f)
@@ -47,7 +48,8 @@ for subid in tqdm(subids, total=len(subids)):
         os.makedirs(ses1_dir)
     if not (ses1_dir / "left").exists():
         scan1 = list(work_dir.glob(f"{subject}_input0000*_mniWarped-WarpedToTemplate.nii.gz"))[0]
-        scan1_dst = ses1_dir / "t1.nii.gz"
+        # scan1_dst = ses1_dir / "t1.nii.gz"
+        scan1_dst = ses1_dir / scan1.name
         if not scan1_dst.exists():
             shutil.copyfile(scan1, scan1_dst)
         cmd = ["bash", hipsthomas_script, str(ses1_dir), scan1_dst.name]
@@ -65,7 +67,8 @@ for subid in tqdm(subids, total=len(subids)):
         os.makedirs(ses2_dir)
     if not (ses2_dir / "left").exists():
         scan2 = list(work_dir.glob(f"{subject}_input0001*_mniWarped-WarpedToTemplate.nii.gz"))[0]
-        scan2_dst = ses2_dir / "t1.nii.gz"
+        # scan2_dst = ses2_dir / "t1.nii.gz"
+        scan2_dst = ses2_dir / scan2.name
         if not scan2_dst.exists():
             shutil.copyfile(scan2, scan2_dst)
         cmd = ["bash", hipsthomas_script, str(ses2_dir), scan2_dst.name]
